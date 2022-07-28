e8588387b5be69c5d97ec01d5ea3f6ca258dff47
hankakee <juniorhankakee@gmail.com>
2022-07-27 23:31:04 -0700

    add buttons

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Poppins',
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        // primarySwatch: Colors.red,
       ),
       home: const MyHomePage(title: 'Chanje lajan'),
     );
@@ -35,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -52,7 +33,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int selectedIndex = 1;
-  String dropdownValue = 'One';
+  String dropdownOrigin = 'One';
+  String dropdownDestination = 'One';
   TextEditingController firstCurrency = TextEditingController();
   void _incrementCounter() {
     setState(() {
@@ -76,130 +58,199 @@ class _MyHomePageState extends State<MyHomePage> {
     double heightscreen = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      // appBar: AppBar(
-      //   // Here we take the value from the MyHomePage object that was created by
-      //   // the App.build method, and use it to set our appbar title.
-      //   title: Text(widget.title),
-      // ),
       body: Center(
         child: Container(
-          padding: const EdgeInsets.only(top: 60.0),
-          height: heightscreen,
-          width: double.infinity,
-          child: Column(
-            children: [
-              Container(
-                // color: Colors.blue,
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Row(
-                  // shrinkWrap: true,
-                  // crossAxisCount: 2,
-                  // mainAxisSpacing: 1.0,
-                  // crossAxisSpacing: 1.0,
-                  // padding: const EdgeInsets.all(0.0),
+          padding: const EdgeInsets.only(top: 80.0),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            height: heightscreen,
+            width: double.infinity,
+            child: Column(
+              children: [
+                Container(
+                  // color: Colors.blue,
+                  // padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 120.0,
+                        child: DropdownButtonFormField<String>(
+                          value: dropdownOrigin,
+                          icon: const Icon(
+                            Icons.keyboard_arrow_down_outlined,
+                            size: 24,
+                          ),
+                          // elevation: 50,
+                          style:
+                              const TextStyle(color: Colors.blue, fontSize: 20),
+                          decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                  borderSide: const BorderSide(
+                                      width: 2, color: Colors.blue))),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownOrigin = newValue!;
+                            });
+                          },
+                          items: <String>['One', 'Two', 'Free', 'Four']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        width: 120,
+                        child: DropdownButtonFormField<String>(
+                          value: dropdownDestination,
+                          icon: const Icon(
+                            Icons.keyboard_arrow_down_outlined,
+                            size: 28,
+                          ),
+                          elevation: 50,
+                          style:
+                              const TextStyle(color: Colors.blue, fontSize: 20),
+                          decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                  borderSide: const BorderSide(
+                                      width: 2, color: Colors.blue))),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownDestination = newValue!;
+                            });
+                          },
+                          items: <String>['One', 'Two', 'Free', 'Four']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  // padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  padding: const EdgeInsets.only(top: 30.0, bottom: 20.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        child: const Text(
+                          "323892",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            top: BorderSide(width: 2.0, color: Colors.white),
+                            bottom: BorderSide(width: 2.0, color: Colors.black),
+                          ),
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Spacer(),
+                      Container(
+                          child:
+                              const Text("0", style: TextStyle(fontSize: 28)))
+                    ],
+                  ),
+                ),
+                GridView.count(
+                  primary: false,
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.all(10),
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  crossAxisCount: 3,
                   children: <Widget>[
-                    DropdownButtonHideUnderline(
-                      child: DropdownButton<String>(
-                        value: dropdownValue,
-                        icon: const Icon(
-                          Icons.keyboard_arrow_down_outlined,
-                          size: 24,
-                        ),
-                        // elevation: 50,
-                        style: const TextStyle(
-                            color: Colors.deepPurple, fontSize: 28),
-                        underline: Container(
-                          height: 2,
-                          color: const Color.fromARGB(255, 0, 0, 0),
-                        ),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownValue = newValue!;
-                          });
-                        },
-                        items: <String>['One', 'Two', 'Free', 'Four']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                    const Spacer(),
-                    DropdownButtonHideUnderline(
-                      child: DropdownButton<String>(
-                        value: dropdownValue,
-                        icon: const Icon(
-                          Icons.keyboard_arrow_down_outlined,
-                          size: 24,
-                        ),
-                        // elevation: 50,
-                        style: const TextStyle(
-                            color: Colors.deepPurple, fontSize: 28),
-                        underline: Container(
-                          height: 2,
-                          color: const Color.fromARGB(255, 0, 0, 0),
-                        ),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownValue = newValue!;
-                          });
-                        },
-                        items: <String>['One', 'Two', 'Free', 'Four']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding:
-                    const EdgeInsets.only(top: 20.0, left: 30.0, right: 30.0),
-                child: Row(
-                  children: [
-                    Flexible(
-                      child: TextFormField(
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        controller: firstCurrency,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
-                        ),
-                        // decoration: const InputDecoration(
-                        //   icon: Icon(Icons.lock, color: const Color(0xFF994CFC)),
-                        //   // border: InputBorder.none,
-                        // ),
-                      ),
-                    ),
-                    const Spacer(),
                     Container(
-                        // color: Colors.amber,
-                        // padding: const EdgeInsets.only(top: 10.0),
-                        child: const Text("0", style: TextStyle(fontSize: 28)))
+                      padding: const EdgeInsets.all(0),
+                      color: Colors.teal[100],
+                      child: Center(
+                        child: const Text("7"),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(0),
+                      color: Colors.teal[200],
+                      child: Center(
+                        child: const Text('8'),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(0),
+                      color: Colors.teal[300],
+                      child: Center(
+                        child: const Text('9'),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(0),
+                      color: Colors.teal[300],
+                      child: Center(
+                        child: const Text('6'),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(0),
+                      color: Colors.teal[300],
+                      child: Center(
+                        child: const Text('5'),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(0),
+                      color: Colors.teal[300],
+                      child: Center(
+                        child: const Text('4'),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(0),
+                      color: Colors.teal[300],
+                      child: Center(
+                        child: const Text('3'),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(0),
+                      color: Colors.teal[300],
+                      child: Center(
+                        child: const Text('2'),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(0),
+                      color: Colors.teal[300],
+                      child: Center(
+                        child: const Text('1'),
+                      ),
+                    ),
                   ],
-                ),
-              )
-            ],
+                )
+              ],
+            ),
+            //  Text(
+            //   '$_counter',
+            //   style: Theme.of(context).textTheme.headline4,
+            // ),
           ),
-          //  Text(
-          //   '$_counter',
-          //   style: Theme.of(context).textTheme.headline4,
-          // ),
         ),
         // child:
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
       bottomNavigationBar: BottomNavigationBar(
           backgroundColor: const Color(0XFFEC3496),
           type: BottomNavigationBarType.shifting,
